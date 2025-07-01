2ed6f35e45bea10abcd553b999616a9d8be520e5
mishmashi <ana.perez.bme@gmail.com>
2025-06-30 19:21:06 -0600

    Update anophenator.py

--- anophenator.py
+++ anophenator.py
@@ -58,13 +58,13 @@ def filter_candidates(index, ans):
 # ---- Main Loop ----
 while st.session_state.index < len(questions):
     if len(st.session_state.candidates) <= 1:
-        st.session_state.index += 1 #skip question if candidate has been picked
+        st.session_state.index += 1 #skip question if candidate has been picked, or all have been eliminated
 
     # Get all values for this question
     #values = {c.get(st.session_state.index, -1) for c in st.session_state.candidates}
 
     # Skip q if all candidates have the same value, so no impurity gain
-    if sum(1 for c in st.session_state.candidates if st.session_state.index in c) <=0:
+    if sum(1 for c in st.session_state.candidates if st.session_state.index in c) <=1:
         st.session_state.index += 1
     else:
         break  
