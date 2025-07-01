606ad64098e698e1c9d91b2a5bf6f8e5f77fbfb4
mishmashi <ana.perez.bme@gmail.com>
2025-06-30 19:53:25 -0600

    Update anophenator.py

--- anophenator.py
+++ anophenator.py
@@ -59,7 +59,6 @@ def filter_candidates(index, ans):
 while st.session_state.index < len(questions):
     if len(st.session_state.candidates) <= 1:
         st.session_state.index += 1 #skip question if candidate has been picked, or all have been eliminated
-        continue
     # Get all values for this question
     #values = {c.get(st.session_state.index, -1) for c in st.session_state.candidates}
 
