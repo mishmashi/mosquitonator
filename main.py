d58aae6086e81506786c3e32e924b8de45b95183
mishmashi <ana.perez.bme@gmail.com>
2025-06-30 21:54:23 -0600

    Update main.py

--- main.py
+++ main.py
@@ -249,7 +249,7 @@ elif st.session_state.phase == "species":
                 for name in st.session_state.others:
                         st.write(f"- **Anopheles {name}**")
         
-        if st.button("🔄 Restart from Genus",key="restart_sp"):
+        if st.button("🔄 Restart from Genus",key="restart_all"):
             st.session_state.index = 0
             st.session_state.candidates = []
             st.session_state.others = []
