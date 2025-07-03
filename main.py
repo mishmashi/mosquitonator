71aa240cd5dac58c687b346002645159db30ccee
mishmashi <ana.perez.bme@gmail.com>
2025-07-03 12:53:06 -0600

    Update main.py

--- main.py
+++ main.py
@@ -175,7 +175,6 @@ elif st.session_state.phase == "species":
             if st.button("Start with prior",key="prior"):
                 prior = st.text_input("Enter prior in format '0,1,0,None,...'")
                 prior = [p for p in prior.split(",")]
-                st.warning(f"prior: {prior}")
                 
                 if st.button("Submit"):
                     st.session_state.prior = prior
@@ -183,7 +182,8 @@ elif st.session_state.phase == "species":
                     for idx, el in enumerate(st.session_state.prior):
                         st.session_state.candidates = filter_candidates(idx, el, st.session_state.candidates)
                         st.write(f"Looped through Q{idx}")
-                        st.warning(f"st.Prior: {st.session_state.prior}")
+                        
+        st.warning(f"st.Prior: {st.session_state.prior}, prior: {prior}, idx: {idx}")
 
             
         st.markdown("Answer the following morphological questions to identify the species of Anopheles:")
