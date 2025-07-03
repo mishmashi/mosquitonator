0067a5263715f91410e23206266692dc9711c1ae
mishmashi <ana.perez.bme@gmail.com>
2025-07-03 12:54:30 -0600

    Update main.py

--- main.py
+++ main.py
@@ -178,12 +178,12 @@ elif st.session_state.phase == "species":
                 
                 if st.button("Submit"):
                     st.session_state.prior = prior
-                    #st.markdown(f"Type: {type(prior)}")
+                    st.markdown(f"prior: {st.session_state.prior}")
                     for idx, el in enumerate(st.session_state.prior):
                         st.session_state.candidates = filter_candidates(idx, el, st.session_state.candidates)
                         st.write(f"Looped through Q{idx}")
                         
-        st.warning(f"st.Prior: {st.session_state.prior}, prior: {prior}, idx: {idx}")
+        st.warning(f"st.Prior: {st.session_state.prior}, prior: {prior}")
 
             
         st.markdown("Answer the following morphological questions to identify the species of Anopheles:")
