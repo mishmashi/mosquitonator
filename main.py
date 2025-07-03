4043006deef45c66b62682e9c3f9c80f29235b9d
mishmashi <ana.perez.bme@gmail.com>
2025-07-03 13:03:36 -0600

    Update main.py

--- main.py
+++ main.py
@@ -190,7 +190,7 @@ elif st.session_state.phase == "species":
         
         # ---- Main Loop ----
         while st.session_state.index < len(questions):
-            if len(st.session_state.candidates) <= 1 or len(prior) >0:
+            if len(st.session_state.candidates) <= 1 or len(st.session_state.prior) >0:
                 st.session_state.index += 1 #skip question if candidate has been picked, or all have been eliminated
             # Get all values for this question
             #values = {c.get(st.session_state.index, -1) for c in st.session_state.candidates}
