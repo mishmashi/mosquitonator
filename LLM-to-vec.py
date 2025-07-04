45e9724e58cbd3ab6592f32c267a471601db9688
mishmashi <ana.perez.bme@gmail.com>
2025-07-04 17:00:53 -0600

    Update LLM-to-vec.py

--- LLM-to-vec.py
+++ LLM-to-vec.py
@@ -94,8 +94,8 @@ if st.button("Submit"):
             ]
          )
         st.session_state.result= response.choices[0].message.content
-        st.write(response.choices[0].message.content)
         st.rerun()
+st.write(response.choices[0].message.content)
 # user_input = st.text_area("Describe the mosquito in detail:", placeholder="The wings are pale, the first main dark area of the costa has a pale interruption...")
 # API call
 #response = client.chat.completions.create(
