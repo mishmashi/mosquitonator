aefe6ed0809df9fd8a4ed796ad0a033070c91a41
mishmashi <ana.perez.bme@gmail.com>
2025-07-25 17:15:23 -0400

    Update colombianator.py

--- colombianator.py
+++ colombianator.py
@@ -143,10 +143,10 @@ if st.session_state.index < len(questions):
 else:
     if len(st.session_state.candidates) == 1:
         st.success(f"The specimen is an **Anopheles {st.session_state.candidates[0]['name']}**")
-    elif len(st.session_state.candidates) > 1:
-        if st.session_state.candidates[0] != 'na':
+    elif len(st.session_state.candidates != 'nan') > 1:
+        if st.session_state.candidates:
             st.warning(f"index: {st.session_state.index}, Possible species:")
-            for c in st.session_state.candidates:
+            for c in st.session_state.candidates != 'nan':
                 st.write(f"- **Anopheles {c['name']}**")
     else:
         st.error("No matching vectors.")
