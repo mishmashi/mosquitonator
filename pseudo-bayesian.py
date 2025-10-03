626aa6ae6495e72b825785182e5e6b087348ec3d
mishmashi <ana.perez.bme@gmail.com>
2025-10-03 17:37:48 -0400

    Update pseudo-bayesian.py

--- pseudo-bayesian.py
+++ pseudo-bayesian.py
@@ -39,8 +39,6 @@ def update_probabilities(ans, index, candidates, thresh, factor=.25):
                   st.session_state.max_prob = candidate["prob"]
               continue
           candidate["prob"] = candidate["prob"]*1.1
-          if candidate["prob"] > st.session_state.max_prob:
-              st.session_state.max_prob = candidate["prob"]
       if st.session_state.max_prob != 0:
           for i, candidate in enumerate(candidates):
               candidate["prob"] = candidate["prob"]/st.session_state.max_prob
