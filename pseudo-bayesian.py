d13a1e94dbd200dc3e6e2a4c1a646b24d4a14dc9
mishmashi <ana.perez.bme@gmail.com>
2025-10-03 14:51:49 -0400

    Update pseudo-bayesian.py

--- pseudo-bayesian.py
+++ pseudo-bayesian.py
@@ -31,7 +31,8 @@ def update_probabilities(ans, index, candidates, thresh, factor=.25):
     c_ans = candidate.get(index, np.nan)
     if pd.isna(c_ans) or ans == c_ans:
       just_el.append(0)
-      candidate["prob"] = candidate["prob"]*1.1
+      if ans == c_ans:
+          candidate["prob"] = candidate["prob"]*1.1
       continue
     elif ans != c_ans:
       candidate["prob"] = candidate["prob"]*factor
