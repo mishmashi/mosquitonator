a228eea005f21dd65ce514799cb7707902094287
mishmashi <ana.perez.bme@gmail.com>
2025-10-03 16:34:57 -0400

    Update pseudo-bayesian.py

--- pseudo-bayesian.py
+++ pseudo-bayesian.py
@@ -132,11 +132,12 @@ if st.session_state.prior:
 _, mid, _ = st.columns(3)
 mid.write(f"**Remaining candidates:** {len(st.session_state.candidates)}")
 
+if st.session_state.prior:
+            if st.button("Skip to ranking", key="skip", use_container_width=True):
+                st.session_state.index = len(questions)
+                
 if not st.session_state.clicked_back:
     while st.session_state.index < len(questions):
-        if st.session_state.prior:
-            if st.button("Skip to ranking", key="skip_2r", use_container_width=True):
-                st.session_state.index = len(questions)
         # Skip uninformative questions
         values = {c.get(st.session_state.index, -1) for c in st.session_state.candidates}
         num_with_values = sum(1 for c in st.session_state.candidates if st.session_state.index in c)
