3b188e4d911cd0f0fe0ad60e2fbf2da3cf9b89bc
mishmashi <ana.perez.bme@gmail.com>
2025-12-30 04:15:01 -0500

    Update candidate probability logic in dbnhybrid.py

--- dbnhybrid.py
+++ dbnhybrid.py
@@ -62,9 +62,9 @@ def update_probabilities(ans, index, candidates, thresh, factor=.25):
     c_ans = candidate.get(index, np.nan)
     if pd.isna(c_ans) or ans == c_ans:
       just_el.append(0)
-      if ans == c_ans and not pd.isna(c_ans):  
+      if ans == c_ans and not pd.isna(c_ans) and c_ans != -1:  
         candidate["prob"] = candidate["prob"]*1.1
-    elif ans != c_ans:
+    elif ans != c_ans and c_ans != -1:
       candidate["prob"] = candidate["prob"]*factor
       if candidate["prob"] < thresh:
         candidate["considered"] = 0
